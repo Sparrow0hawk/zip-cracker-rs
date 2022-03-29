65b88bee236a8ae74860753863f02a0d05317267
Sparrow0hawk <alexjcoleman@gmail.com>
2022-03-29 08:15:49 +0100

    created alphabeta module

--- src/alphabet.rs
+++ src/alphabet.rs
@@ -1,8 +1,9 @@
-fn main() {
+// function for retrieving upper and lowercase characters
+pub fn get_alphabet() -> Vec<char> {
     let alphabet = (b'A'..=b'z')
         .map(|c| c as char)
         .filter(|c| c.is_alphabetic())
         .collect::<Vec<_>>();
 
-    println!("{:?}", alphabet)
+    return alphabet;
 }
