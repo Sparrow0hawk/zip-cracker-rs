e948572405017b18196e0373748f125e221b9c23
Sparrow0hawk <alexjcoleman@gmail.com>
2022-03-29 09:02:45 +0100

    adding loop for creating new mutated strings

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,24 @@ fn main() {
 
     let password_chars: Vec<char> = password.chars().collect();
 
-    println!("{:?}", password_chars)
-    //    for (i,c) in password.chars().enumerate() {
-    //
-    //    }
+    println!("{:?}", password_chars);
+
+    let mut passwords: Vec<String> = vec![];
+
+    for (i, c) in password.chars().enumerate() {
+        for letter in characters.iter() {
+            // create a cloned vector of password characters
+            let mut trial_pwd = password_chars.clone();
+
+            // change character at ith index using letter var
+            trial_pwd[i] = *letter;
+
+            // create a new string from the trial_pwd vec with mutated char
+            let trial_pwd_str: String = trial_pwd.into_iter().collect();
+
+            // append this string to passwords Vec
+            passwords.push(trial_pwd_str);
+        }
+    }
+    println!("{:?}", passwords);
 }
